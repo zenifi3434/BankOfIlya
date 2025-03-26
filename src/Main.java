b79537612f0ec6bc78c7d1aa32604283c971d125
iseyf <c4033962@newcastle.ac.uk>
2025-03-26 11:52:56 +0000

    Created inital I/O of login of app, with password encryption

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,40 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+        boolean valid = false;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("banking app");
+        System.out.println("Create your password");
+        String password = scanner.nextLine();
+        System.out.println("Password Created!");
+        System.out.println();
+
+
+        while (!valid) {
+
+            System.out.println("Enter your password");
+            String passattempt = scanner.nextLine();
+            if (!password.equalsIgnoreCase(passattempt)) {
+                System.out.println("XXX INVALID PASSWORD, TRY AGAIN");
+            } else if (password.equalsIgnoreCase(passattempt)) {
+                System.out.println("Welcome: ");
+                valid = true;
+            }
+
+
+        }
+
+        while (valid) {
+            System.out.println("1) Load an Account");
+            System.out.println("2) Open a new Account");
+
+
+
+
+        }
+
+
+    }
 }
